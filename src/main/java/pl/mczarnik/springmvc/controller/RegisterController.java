7a9515cd467d6abd5767fba98c515e5c39723001
Mateusz Czarnik <mamarok@gmail.com>
2018-09-13 15:48:37 +0200

    Add register controller and view

---
+++ src/main/java/pl/mczarnik/springmvc/controller/RegisterController.java
@@ -0,0 +1,67 @@
+package pl.mczarnik.springmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import pl.mczarnik.springmvc.model.User;
+
+import javax.validation.Valid;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    @Autowired
+    private UserService userService;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/show")
+    public String showRegister(Model model) {
+        model.addAttribute("User", new User());
+
+        return "register";
+    }
+
+    @PostMapping("/process")
+    public String processRegister(
+            @Valid @ModelAttribute("User") User user,
+            BindingResult bindingResult,
+            Model model) {
+
+        String userName = user.getUserName();
+        logger.info("Processing registration form for: " + userName);
+
+        // form validation
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        // check the database if user already exists
+        User existing = userService.findByUserName(userName);
+        if (existing != null) {
+            model.addAttribute("user", new User());
+            model.addAttribute("registrationError", "User name already exists.");
+
+            logger.warning("User name already exists.");
+            return "register";
+        }
+        // create user account
+        userService.save(user);
+
+        logger.info("Successfully created user: " + userName);
+
+        return "registration-confirmation";
+    }
+}
